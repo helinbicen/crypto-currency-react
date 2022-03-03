a1a7922e5546dc4fa6ed04341a1a16736af714de
Helin Biçen <71695516+helinbicen@users.noreply.github.com>
2022-03-03 21:01:56 +0300

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <Link to="/">
+      <div>
+        <div className="navbar">
+          <FaCoins className="icon" />
+          <h1>
+            Coin <span className="purple">Search</span>
+          </h1>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Navbar;
